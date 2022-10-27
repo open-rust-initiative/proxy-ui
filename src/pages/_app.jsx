69590584e1dd0a72670d88a84119c08dc9e9482b
Quanyi Ma <eli@patch.sh>
2022-10-27 21:23:11 +0800

    Fix the GitHub Action build error
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,6 +8,8 @@ import { Layout } from '@/components/Layout'
 import 'focus-visible'
 import '@/styles/tailwind.css'
 
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
+
 function getNodeText(node) {
   let text = ''
   for (let child of node.children ?? []) {
@@ -65,17 +67,17 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-W6F289JN4D`} />
-
-      <Script strategy="lazyOnload">
+      <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-W6F289JN4D', {
-            page_path: window.location.pathname,
-          });
-        `}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
       </Script>
 
       <Head>
